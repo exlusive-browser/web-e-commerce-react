ce2b10f6a114fca0f4788d85baa0871f7b58b8ae
Juan Diego García <salasjuan@uninorte.edu.co>
2024-09-21 16:09:30 -0500

    fix(HU2013): decorations and responsive design

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -4,7 +4,6 @@ import ProductCard from "./components/ProductCard";
 export function ProductList() {
   return (
     <MainLayout>
-      <p>ProductList</p>
       <ProductCard
         products={[
           {
@@ -16,19 +15,91 @@ export function ProductList() {
             img: "https://via.placeholder.com/150",
           },
           {
-            name: "Product 2",
-            rating: 3,
-            price: 2000,
-            discount: 10,
-            description: "Description 2",
+            name: "APPLE 12.9-Inch LTE 128GB Storage 8GB RAM",
+            rating: 5,
+            price: 6869000,
+            discount: 25,
+            description: "Large-screen LTE tablet with 128 GB storage and 8 GB RAM, perfect for productivity and entertainment on the go.",
             img: "https://via.placeholder.com/150",
           },
           {
-            name: "Product 3",
+            name: "XIAOMI Redmi Note 13 Pro 256GB Storage 8GB RAM Green",
+            rating: 4,
+            price: 1999900,
+            discount: 55,
+            description: "High-performance smartphone with 256 GB storage, 8 GB RAM, and a sleek green finish for power users.",
+            img: "https://via.placeholder.com/150",
+          },
+          {
+            name: "SAMSUNG GALAXY Tablet TAB S7 12.4 Inches 128 GB 6 GB RAM",
+            rating: 4,
+            price: 3999900,
+            discount: 33,
+            description: "Large-screen tablet with 128 GB storage and 6 GB RAM, designed for multimedia and productivity.",
+            img: "https://via.placeholder.com/150",
+          },
+          {
+            name: "iPhone 15 128 GB Pink",
             rating: 5,
-            price: 300,
-            discount: 30,
-            description: "Description 3",
+            price: 4139010,
+            discount: 10,
+            description: "Sleek iPhone 15 in a vibrant pink color, with 128 GB storage for all your apps and media.",
+            img: "https://via.placeholder.com/150",
+          },
+          {
+            name: "MOTOROLA Moto G14 128 GB 8 GB RAM Purple",
+            rating: 5,
+            price: 759000,
+            discount: 45,
+            description: "Affordable yet powerful smartphone with 128 GB storage and 8 GB RAM for smooth multitasking.",
+            img: "https://via.placeholder.com/150",
+          },
+          {
+            name: "LENOVO M9 Tablet 9-Inch 64GB Storage 4GB RAM Arctic Grey",
+            rating: 5,
+            price: 949000,
+            discount: 53,
+            description: "Compact tablet with 64 GB storage and 4 GB RAM, ideal for entertainment and light productivity.",
+            img: "https://via.placeholder.com/150",
+          },
+          {
+            name: "APPLE Watch Ultra 2 49mm Titanium Blue MREG3BE/A",
+            rating: 4,
+            price: 4799900,
+            discount: 31,
+            description: "Advanced smartwatch with 49mm titanium build, designed for durability and performance tracking.",
+            img: "https://via.placeholder.com/150",
+          },
+          {
+            name: "Apple iPad Mini 6th Generation 2021 8.3 64GB Starlight Silver",
+            rating: 4,
+            price: 2998000,
+            discount: 31,
+            description: "Compact iPad Mini with 64 GB storage, perfect for portability without compromising on performance.",
+            img: "https://via.placeholder.com/150",
+          },
+          {
+            name: "Redmi Watch 4 Gray XIAOMI Watch 4",
+            rating: 4,
+            price: 515000,
+            discount: 10,
+            description: "Stylish and functional smartwatch with a sleek design and essential health-tracking features.",
+            img: "https://via.placeholder.com/150",
+          },
+          {
+            name: "Samsung Galaxy A55 5G 256GB 8GB RAM Purple",
+            rating: 4,
+            price: 2099900,
+            discount: 34,
+            description: "High-performance smartphone with 5G connectivity, 256 GB storage, and 8 GB RAM for seamless multitasking.",
+            img: "https://via.placeholder.com/150",
+          },
+          {
+            name: "HUAWEI nova 10 Pro 256GB 8GB RAM Gray",
+            rating: 4,
+            price: 3699900,
+            discount: 28,
+            description: "Flagship smartphone with 256 GB storage, 8 GB RAM, and a stunning gray finish, built for power users.",
             img: "https://via.placeholder.com/150",
           },
         ]} />
--- src/pages/ProductList/components/ProductCard.tsx
+++ src/pages/ProductList/components/ProductCard.tsx
@@ -2,54 +2,64 @@ import { StarsRating } from "./StarsRating";
 import cartIcon from "@/assets/plp/cart.svg";
 
 interface Product {
-    name: string;
-    rating: number;
-    price: number;
-    discount: number;
-    description: string;
-    img: string;
-  }
-  
-  interface ProductListProps {
-    products: Product[];
-  }
-  
-  const ProductCard: React.FC<ProductListProps> = ({ products }) => {
-    return (
-      <div>
-        {products.map((product, index) => {
-          const discountedPrice = (product.price - (product.price * product.discount) / 100).toFixed(2);
-  
-          return (
-            <div key={index} className="flex h-[300px] mb-10 mx-5 rounded-xl overflow-hidden transition-transform transform hover:scale-105 shadow-md">
-              <img src={product.img} alt={product.name} className="mr-5"/>
-              <div className="my-3">
-                <h2 className="text-3xl mb-1">{product.name}</h2>
+  name: string;
+  rating: number;
+  price: number;
+  discount: number;
+  description: string;
+  img: string;
+}
+
+interface ProductListProps {
+  products: Product[];
+}
+
+const ProductCard: React.FC<ProductListProps> = ({ products }) => {
+  return (
+    <div className="flex flex-col justify-center gap-8">
+      {products.map((product, index) => {
+        const discountedPrice = (product.price - (product.price * product.discount) / 100).toFixed(2);
+
+        return (
+          <div
+            key={index}
+            className="flex flex-col md:flex-row mx-3 md:w-[700px] lg:w-[800px] mb-10 cursor-pointer rounded-2xl overflow-hidden transition-transform transform hover:scale-105 shadow-md">
+            <img src={product.img} alt={product.name} className="w-full md:w-2/3 object-cover" />
+            <div className="p-5 flex flex-col justify-between">
+              <div>
+                <h2 className="text-xl md:text-xl my-1">{product.name}</h2>
                 <StarsRating rating={product.rating} />
-                <div className="flex gap-4 mb-5 items-center">
-                  {product.discount > 0 ? (
-                    <>
-                      <p className="text-gray-500 text-2xl font-light line-through decoration-1">${product.price.toLocaleString()}</p>
-                      <p className="text-2xl font-bold">${Number(discountedPrice).toLocaleString()}</p>
-                      <p className="text-[#3498db] font-bold">{product.discount}% off</p>
-                    </>
-                  ) : (
-                    <p className="text-2xl font-bold">${product.price.toLocaleString()}</p>
-                  )}
-                </div>
-                <p className="mb-3">{product.description}</p>
-                <a href="#">
-                  <div className="flex h-12 w-40 rounded-xl bg-[#3498db] gap-3 items-center justify-center">
-                    <img src={cartIcon} alt="Add to cart" className="h-7"/>
-                    <p className="text-white">Add to cart</p>
-                  </div>
-                </a>
               </div>
+              <div className="flex flex-col md:flex-row gap-2 md:gap-4 mb-5 items-start md:items-center">
+                {product.discount > 0 ? (
+                  <>
+                    <p className="text-gray-500 text-xl md:text-2xl font-light line-through decoration-1">
+                      ${product.price.toLocaleString()}
+                    </p>
+                    <p className="text-xl md:text-2xl font-bold">
+                      ${Number(discountedPrice).toLocaleString()}
+                    </p>
+                    <p className="text-[#3498db] font-bold">{product.discount}% off</p>
+                  </>
+                ) : (
+                  <p className="text-xl md:text-2xl font-bold">${product.price.toLocaleString()}</p>
+                )}
+              </div>
+              <p className="mb-3">{product.description}</p>
+              <a href="#">
+                <div className="flex h-12 w-full md:w-[160px] rounded-xl bg-[#3498db] gap-3 items-center justify-center">
+                  <img src={cartIcon} alt="Add to cart" className="h-7" />
+                  <p className="text-white">Add to cart</p>
+                </div>
+              </a>
             </div>
-          );
-        })}
-      </div>
-    );
-  };
-  
-  export default ProductCard;
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+
+
+export default ProductCard;
