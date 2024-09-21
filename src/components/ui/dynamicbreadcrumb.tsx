36b70031912b639b3b040cbe928733d26d5ec584
Alejandro Salas <ainfantem@uninorte.edu.co>
2024-09-21 11:17:28 -0500

    feat(UH2009): requested corrections added

--- src/components/ui/dynamicbreadcrumb.tsx
+++ src/components/ui/dynamicbreadcrumb.tsx
@@ -8,9 +8,16 @@ import {
   BreadcrumbSeparator
 } from "@/components/ui/breadcrumb";
 
+function slugToTitle(slug: string) {
+    return slug
+    .split("-")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
 const DynamicBreadcrumb = () => {
     const location = useLocation();
-    const pathnames = location.pathname.split("/").filter(x => x);
+    const pathnames = location.pathname.split("/").filter((x) => x);
 
     return (
         <Breadcrumb className="breadcrumb list-none flex py-2.5 ml-5 sm:ml-10">
@@ -28,10 +35,10 @@ const DynamicBreadcrumb = () => {
                             <BreadcrumbSeparator />
                             <BreadcrumbItem>
                                 {isLast ? (
-                                    <span className="font-normal text-foreground">{name}</span>
+                                    <span className="font-normal text-foreground">{slugToTitle(name)}</span>
                                 ) : (
                                     <BreadcrumbLink asChild>
-                                        <Link to={routeTo}>{name}</Link>
+                                        <Link to={routeTo}>{slugToTitle(name)}</Link>
                                     </BreadcrumbLink>
                                 )}
                             </BreadcrumbItem>
