6dd72c406b4a3942553001929361d516d9a6d9f1
Juan Diego García <salasjuan@uninorte.edu.co>
2024-09-20 23:54:20 -0500

    feat(HU2013): add product cards

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,11 +1,37 @@
 import { MainLayout } from "@/layout/MainLayout";
-import { StarsRating } from "./components/StarsRating";
+import ProductCard from "./components/ProductCard";
 
 export function ProductList() {
   return (
     <MainLayout>
       <p>ProductList</p>
-      <StarsRating rating={5} />
+      <ProductCard
+        products={[
+          {
+            name: "iPhone 15 Pro Max 256 GB Natural Titanium",
+            rating: 4,
+            price: 5899000,
+            discount: 17,
+            description: "Premium smartphone with a sleek titanium finish, 256 GB storage, and advanced camera features for a superior mobile experience.",
+            img: "https://via.placeholder.com/150",
+          },
+          {
+            name: "Product 2",
+            rating: 3,
+            price: 2000,
+            discount: 10,
+            description: "Description 2",
+            img: "https://via.placeholder.com/150",
+          },
+          {
+            name: "Product 3",
+            rating: 5,
+            price: 300,
+            discount: 30,
+            description: "Description 3",
+            img: "https://via.placeholder.com/150",
+          },
+        ]} />
     </MainLayout>
   );
 }
--- src/pages/ProductList/components/ProductCard.tsx
+++ src/pages/ProductList/components/ProductCard.tsx
@@ -1,5 +1,55 @@
-export function ProductCard() {
+import { StarsRating } from "./StarsRating";
+import cartIcon from "@/assets/plp/cart.svg";
+
+interface Product {
+    name: string;
+    rating: number;
+    price: number;
+    discount: number;
+    description: string;
+    img: string;
+  }
+  
+  interface ProductListProps {
+    products: Product[];
+  }
+  
+  const ProductCard: React.FC<ProductListProps> = ({ products }) => {
     return (
-        <></>
+      <div>
+        {products.map((product, index) => {
+          const discountedPrice = (product.price - (product.price * product.discount) / 100).toFixed(2);
+  
+          return (
+            <div key={index} className="flex h-[300px] mb-10 mx-5 rounded-xl overflow-hidden transition-transform transform hover:scale-105 shadow-md">
+              <img src={product.img} alt={product.name} className="mr-5"/>
+              <div className="my-3">
+                <h2 className="text-3xl mb-1">{product.name}</h2>
+                <StarsRating rating={product.rating} />
+                <div className="flex gap-4 mb-5 items-center">
+                  {product.discount > 0 ? (
+                    <>
+                      <p className="text-gray-500 text-2xl font-light line-through decoration-1">${product.price.toLocaleString()}</p>
+                      <p className="text-2xl font-bold">${Number(discountedPrice).toLocaleString()}</p>
+                      <p className="text-[#3498db] font-bold">{product.discount}% off</p>
+                    </>
+                  ) : (
+                    <p className="text-2xl font-bold">${product.price.toLocaleString()}</p>
+                  )}
+                </div>
+                <p className="mb-3">{product.description}</p>
+                <a href="#">
+                  <div className="flex h-12 w-40 rounded-xl bg-[#3498db] gap-3 items-center justify-center">
+                    <img src={cartIcon} alt="Add to cart" className="h-7"/>
+                    <p className="text-white">Add to cart</p>
+                  </div>
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     );
-}
+  };
+  
+  export default ProductCard;
