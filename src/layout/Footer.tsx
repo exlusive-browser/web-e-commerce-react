c801ecc99fd01af4d04adf2a0ddd704ae90dcc39
sebastianBarandica <sebastianbarandica@uninorte.edu.co>
2024-09-22 10:45:38 -0500

    fix/responsive footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -9,10 +9,10 @@ export function Footer() {
   return (
     <footer className="block w-full  bg-[#1c2833]  text-white  p-6 ">
       <div className="w-full block">
-      <div className="flex justify-between items-center px-8 mb-6 gap-8">
+      <div className="flex flex-col sm:flex-row justify-center md:justify-between items-center px-8 mb-6 gap-8">
         <div className="m-0 text-[20px] font-sans font-bold">
           <h3>Up Ecommerce</h3>
-          <p className="my-1.5 text-[14px]">NIT: 123456789</p>
+          <p className="my-1.5 text-[14px] text-center">NIT: 123456789</p>
         </div>
         <div className="flex mx-2 no-underline text-[16px] space-x-4">
           <a href="facebook.com">
@@ -42,7 +42,7 @@ export function Footer() {
         </div>
         <div className="flex flex-col gap-2.5">
           <p className="flex flex-col gap-2.5 font-bold">Payment Methods</p>
-          <div className="flex gap-2.5">
+          <div className="flex gap-2.5 justify-center">
             <img src={visa} alt="Visa logo" width="32" height="32" />
             <img
               src={mastercard}
@@ -55,8 +55,8 @@ export function Footer() {
         </div>
       </div>
       </div>
-      <div className="flex justify-center items-center  p-2.5 px-5">
-        <ul className="flex list-none p-0 m-0 text-center space-x-6">
+      <div className="flex justify-center items-center  p-2.5 px-5  ">
+        <ul className="flex list-none p-0 m-0 text-center space-x-6  md: flex-wrap justify-center">
           <li>
             <a href="">Home</a>
           </li>
