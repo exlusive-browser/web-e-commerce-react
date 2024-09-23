57a847dddfe5ae4aec1980f67a470d74b771c00b
dgop92 <inevaup@gmail.com>
2024-09-22 20:53:34 -0500

    chore: integrate landing page images

--- src/pages/Landing/LandingPage.tsx
+++ src/pages/Landing/LandingPage.tsx
@@ -1,12 +1,32 @@
 import { MainLayout } from "@/layout/MainLayout";
 import DynamicBreadcrumb from "@/components/ui/dynamicbreadcrumb";
 import TopsellingProducts from "../Products/TopsellingProducts";
+import PromotionalImages from "@/components/ui/landing-promotional-images";
+import AdBanner from "@/components/ui/landing-ad-banner";
+
+const items = [
+  {
+    image: "src/assets/images/sale1.png",
+    link: "/",
+  },
+  {
+    image: "src/assets/images/sale2.png",
+    link: "/",
+  },
+];
 
 export function LandingPage() {
   return (
     <MainLayout>
       <DynamicBreadcrumb />
+      <AdBanner items={items} />
+      <PromotionalImages
+        item={{ image: "src/assets/images/sale3.png", link: "/" }}
+      />
       <TopsellingProducts />
+      <PromotionalImages
+        item={{ image: "src/assets/images/sale4.png", link: "/" }}
+      />
     </MainLayout>
   );
 }
