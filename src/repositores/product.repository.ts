7e307c062f4d17d4bd9a4e4616507c0f2549be91
dgop92 <inevaup@gmail.com>
2024-10-18 22:32:53 -0500

    fix: change data type of prices in json server db

--- src/repositores/product.repository.ts
+++ src/repositores/product.repository.ts
@@ -1,30 +1,61 @@
 import { Product, SimplifiedProduct } from "@/entities/product-item";
 import { axiosClient } from "@/lib/axios-client";
+import { formatCurrency } from "@/lib/helpers";
+
+type ProductRepoEntity = Product & {
+  originalPrice: number;
+  price: number;
+};
+
+type SimplifiedProductRepoEntity = SimplifiedProduct & {
+  originalPrice: number;
+  price: number;
+};
+
+function fromRepoEntityToProductEntity(product: ProductRepoEntity): Product {
+  return {
+    ...product,
+    price: formatCurrency(product.price),
+    originalPrice: formatCurrency(product.originalPrice),
+  };
+}
+
+function fromRepoEntityToSimplifiedProductEntity(
+  product: SimplifiedProductRepoEntity
+): SimplifiedProduct {
+  return {
+    ...product,
+    price: formatCurrency(product.price),
+    originalPrice: formatCurrency(product.originalPrice),
+  };
+}
 
 export async function getTopSellingProducts(): Promise<SimplifiedProduct[]> {
-  const response = await axiosClient.get<SimplifiedProduct[]>("/top-selling");
-  return response.data;
+  const response = await axiosClient.get<SimplifiedProductRepoEntity[]>(
+    "/top-selling"
+  );
+  return response.data.map(fromRepoEntityToSimplifiedProductEntity);
 }
 
 export async function getProducts(): Promise<Product[]> {
-  const response = await axiosClient.get<Product[]>("/product");
-  return response.data;
+  const response = await axiosClient.get<ProductRepoEntity[]>("/product");
+  return response.data.map(fromRepoEntityToProductEntity);
 }
 
 export async function getProductsByCategory(
   category: string
 ): Promise<Product[]> {
-  const response = await axiosClient.get<Product[]>(
+  const response = await axiosClient.get<ProductRepoEntity[]>(
     `/products?category=${category}`
   );
-  return response.data;
+  return response.data.map(fromRepoEntityToProductEntity);
 }
 
 export async function getProductById(id: number): Promise<Product> {
-  const response = await axiosClient.get<Product>(`/products/${id}`);
+  const response = await axiosClient.get<ProductRepoEntity>(`/products/${id}`);
   const product = response.data; // Ahora es un objeto directamente
 
-  return product;
+  return fromRepoEntityToProductEntity(product);
 }
 
 export async function getRecommendedProducts(
@@ -34,8 +65,8 @@ export async function getRecommendedProducts(
   const baseProduct = await getProductById(productId);
   const id = baseProduct.id;
   const category = baseProduct.category;
-  const response = await axiosClient.get<Product[]>(
+  const response = await axiosClient.get<ProductRepoEntity[]>(
     `/products?category=${category}&id_ne=${id}&_limit=4`
   );
-  return response.data;
+  return response.data.map(fromRepoEntityToProductEntity);
 }
